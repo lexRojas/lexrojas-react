8628c842f8a5fa34acd1dbd95170558cecd610c4
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-08-09 21:50:40 -0600

    cambiuo de lenguaje

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 document.title = 'LexRojas Tools';
+document.documentElement.lang = "es"
 root.render(
   <React.StrictMode>
     <App />
