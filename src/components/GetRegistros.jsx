35cb97202dd4040836057d31d1692ec4f2b3b2ef
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-08-14 11:41:33 -0600

    axxios fullled

--- src/components/GetRegistros.jsx
+++ src/components/GetRegistros.jsx
@@ -22,18 +22,6 @@ const GetRegistros = () => {
   };
 
   
-  // const fetchData = async () => {
-  //   try {
-  //     const response = await fetch('https://lexrojas-backend-c21ed8c2c96d.herokuapp.com/registros');
-  //     const jsonData = await response.json();
-  //     setData(jsonData);
-  //   } catch (error) {
-  //     console.log('Error fetching data:', error);
-  //   }
-  // };
-
-
-
   if (data) { return (
       (data.map((o) => (
           <option key={o.id_registro} value={o.id_registro}> {o.registro_descripcion} </option>
