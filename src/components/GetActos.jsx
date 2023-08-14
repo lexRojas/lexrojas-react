b3161646592737b699aa7ef45e39d45009e16499
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2023-08-14 11:41:14 -0600

    axios full

--- src/components/GetActos.jsx
+++ src/components/GetActos.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-//import axios from "axios";
+import axios from "axios";
 
 export default function GetActos({idRegistro}) {
   const [Actos, setData] = useState(null);
@@ -9,17 +9,20 @@ export default function GetActos({idRegistro}) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [idRegistro]);
 
-  const fetchData = async () => {
-    try {
+  const baseURL = "https://lexrojas-backend-c21ed8c2c96d.herokuapp.com/actos"
 
-      const sql =  "https://lexrojas-backend-c21ed8c2c96d.herokuapp.com/actos?idRegistro=".concat(idRegistro)
-        const response = await fetch(sql);
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.log("Error fetching data:", error);
-    }
+  const fetchData = async () => {
+    await axios.get(baseURL, {params: {idRegistro: idRegistro}})
+    .then(function (response) {
+      setData(response.data)
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error);
+    })  
   };
+
+
   if (Actos) {
     return Actos.map(({ id_acto, acto_descripcion }) => (
       <option key={id_acto} value={id_acto}>
